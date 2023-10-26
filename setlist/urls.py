0dbe51c3b91f1932de910de425c772c0717796f3
Dave St.Germain <dcs@adullmoment.com>
2023-10-26 12:01:53 -0400

    setlist editing

--- setlist/urls.py
+++ setlist/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.show_setlists),
     path("<int:list_id>/", views.show_setlist),
     path("<int:list_id>/duplicate/", views.duplicate_setlist),
+    path("<int:list_id>/edit/", views.edit_setlist),
 ]
--- setlist/views.py
+++ setlist/views.py
@@ -1,7 +1,9 @@
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import user_passes_test
+from django.http import JsonResponse
 
-from .models import SetList
+from .models import SetList, SetSong, Song
 
 
 def show_setlists(request):
@@ -12,9 +14,11 @@ def show_setlists(request):
 
 
 def show_setlist(request, list_id):
+    songlist = get_object_or_404(SetList, pk=list_id)
     context = {
-        "list": get_object_or_404(SetList, pk=list_id),
+        "list": songlist,
         "do_print": request.GET.get("print") == "1",
+        "all_songs": Song.objects.exclude(pk__in=[s.id for s in songlist.songs.all()])
     }
     return render(request, "setlist/setlist.html", context)
 
@@ -22,9 +26,46 @@ def show_setlist(request, list_id):
 def can_duplicate(user):
     return user.has_perm("setlist.add_setlist")
 
+def can_edit(user):
+    return user.has_perm("setlist.change_setlist")
+
 
 @user_passes_test(can_duplicate)
 def duplicate_setlist(request, list_id):
     source = get_object_or_404(SetList, pk=list_id)
     dest = source.duplicate()
-    return redirect(f"/django-admin/setlist/setlist/{dest.id}/change/")
+    return redirect(f"/setlist/{dest.id}/")
+
+
+@user_passes_test(can_edit)
+def edit_setlist(request, list_id):
+    source = get_object_or_404(SetList, pk=list_id)
+    data = json.loads(request.body)
+
+    if data["title"] != source.title:
+        source.title = data["title"]
+
+    new_order = data["order"]
+    songs = list(SetSong.objects.filter(setlist=source))
+    for song in songs:
+        song.sort_order = -song.sort_order
+        song.save()
+
+    while songs:
+        song = songs.pop()
+        try:
+            song.sort_order = new_order.pop(str(song.song_id))
+        except KeyError:
+            song.delete()
+        else:
+            song.save()
+
+    if songs:
+        # something was deleted
+        for song in songs:
+            song.delete()
+    # add missing songs
+    if new_order:
+        for song_id, sort_order in new_order.items():
+            SetSong.objects.create(setlist=source, song_id=int(song_id), sort_order=sort_order)
+    return JsonResponse({"saved": True})

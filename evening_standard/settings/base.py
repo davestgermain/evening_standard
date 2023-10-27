80072d92c4fc17c0ee61d6df53a5bcd85c0d8cd6
Dave St.Germain <dcs@adullmoment.com>
2023-10-27 16:02:15 -0400

    Add sitemap.xml

--- evening_standard/settings/base.py
+++ evening_standard/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
     "mptt",
     "haystack",
     "widget_tweaks",
--- evening_standard/urls.py
+++ evening_standard/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 from search import views as search_views
 from machina import urls as machina_urls
@@ -17,6 +18,7 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
     path("forum/", include(machina_urls)),
     path("setlist/", include("setlist.urls")),
+    path("sitemap.xml", sitemap),
 ]
 
 

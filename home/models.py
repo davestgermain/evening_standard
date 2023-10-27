c2d9e47e6be6ec2d2fdfb69149fa590bd40cfa2b
Dave St.Germain <dcs@adullmoment.com>
2023-10-27 13:31:21 -0400

    Add contact page

--- home/models.py
+++ home/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
 
 class HomePage(Page):
@@ -24,3 +27,32 @@ class RecordingPage(Page):
         from shows.models import Show
 
         return Show.objects.filter(recordings__isnull=False)
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        "ContactPage", on_delete=models.CASCADE, related_name="form_fields"
+    )
+
+
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("from_address", classname="col6"),
+                        FieldPanel("to_address", classname="col6"),
+                    ]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email",
+        ),
+    ]

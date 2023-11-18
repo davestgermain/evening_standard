1590af7b1714a5fcf54e70f475adba4566853111
Dave St.Germain <dcs@adullmoment.com>
2023-11-18 13:21:17 -0500

    Need forwarded address

--- shows/views.py
+++ shows/views.py
@@ -8,7 +8,7 @@ def mark_listen(request):
     doc = get_object_or_404(Document, pk=request.POST.get("document", 0))
 
     doc.listen_set.create(
-        ip_address=request.META["REMOTE_ADDR"],
+        ip_address=request.META.get("HTTP_X_FORWARDED_FOR", "127.0.0.1"),
         browser=request.META["HTTP_USER_AGENT"],
         referer=request.META["HTTP_REFERER"],
     )
